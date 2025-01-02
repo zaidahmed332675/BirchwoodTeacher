799ae468d630c403369b0a65993704c031c3d618
Zaid Bin Ahmed Makharish <zaidahmed332675@gmail.com>
2025-01-02 22:08:08 +0500

    Not found added in both education and experience

--- src/Screens/Profile/index.tsx
+++ src/Screens/Profile/index.tsx
@@ -25,26 +25,26 @@ const Profile = ({ navigation }: Props) => {
     getUserProfile();
   }, [getUserProfile]);
 
-  const profileold = {
-    education: [
-      {
-        instituteName:
-          'Federal Urdu University of Arts, Science & Technology, Karachi.',
-        degree: "Bachelor's degree",
-        startingDate: '2016',
-        endingDate: '2020',
-        fieldOfStudy: 'Computer Science',
-      },
-    ],
-    experience: [
-      {
-        officeName: 'TheCoding Buzz',
-        designation: 'MERN Stack Developer',
-        startingDate: '2016',
-        endingDate: 'Present',
-      },
-    ],
-  };
+  // const profileold = {
+  //   education: [
+  //     {
+  //       instituteName:
+  //         'Federal Urdu University of Arts, Science & Technology, Karachi.',
+  //       degree: "Bachelor's degree",
+  //       startingDate: '2016',
+  //       endingDate: '2020',
+  //       fieldOfStudy: 'Computer Science',
+  //     },
+  //   ],
+  //   experience: [
+  //     {
+  //       officeName: 'TheCoding Buzz',
+  //       designation: 'MERN Stack Developer',
+  //       startingDate: '2016',
+  //       endingDate: 'Present',
+  //     },
+  //   ],
+  // };
 
   if (loading) {
     return <DataLoader />;
@@ -194,66 +194,81 @@ const Profile = ({ navigation }: Props) => {
             }}
             text="Education"
           />
-          {profile.education?.map((edu, ind) => (
-            <View key={'edu-' + ind}>
-              {ind > 0 && (
-                <View
-                  style={
-                    [styles.divider, {
-                    }]
-                  }
-                />
-              )}
-              <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
-                <View style={styles.userInfoItem}>
-                  <GrayMediumText
-                    _style={{
-                      color: colors.theme.primary,
-                    }}
-                    text="Institute Name"
-                  />
-                  <GrayMediumText
-                    _style={{
-                      color: colors.text.black,
-                    }}
-                    text={edu.school}
-                  />
+          {profile.education.length ?
+            <View style={{ marginBottom: 50 }}>
+              {profile.education?.map((edu, ind) => (
+                <View key={'edu-' + ind}>
+                  {ind > 0 && (
+                    <View
+                      style={
+                        [styles.divider, {
+                        }]
+                      }
+                    />
+                  )}
+                  <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+                    <View style={styles.userInfoItem}>
+                      <GrayMediumText
+                        _style={{
+                          color: colors.theme.primary,
+                        }}
+                        text="Institute Name"
+                      />
+                      <GrayMediumText
+                        _style={{
+                          color: colors.text.black,
+                        }}
+                        text={edu.school}
+                      />
+                    </View>
+                    <View style={styles.userInfoItem}>
+                      <GrayMediumText
+                        _style={{
+                          color: colors.theme.primary,
+                        }}
+                        text="Subjects"
+                      />
+                      <GrayMediumText
+                        _style={{
+                          color: colors.text.black,
+                        }}
+                        text={edu.subject.join(', ')}
+                      />
+                    </View>
+                    <View style={styles.userInfoItem}>
+                      <GrayMediumText
+                        _style={{
+                          color: colors.theme.primary,
+                        }}
+                        text="Start - End Date"
+                      />
+                      <GrayMediumText
+                        _style={{
+                          color: colors.text.black,
+                        }}
+                        text={`${format(edu.start, 'yyyy-MM-dd')} - ${isAfter(new Date(), edu.end) ? format(edu.end, 'yyyy-MM-dd') : 'Present'}`}
+                      />
+                    </View>
+                  </View>
                 </View>
-                <View style={styles.userInfoItem}>
-                  <GrayMediumText
-                    _style={{
-                      color: colors.theme.primary,
-                    }}
-                    text="Subjects"
-                  />
-                  <GrayMediumText
-                    _style={{
-                      color: colors.text.black,
-                    }}
-                    text={edu.subject.join(', ')}
-                  />
-                </View>
-                <View style={styles.userInfoItem}>
-                  <GrayMediumText
-                    _style={{
-                      color: colors.theme.primary,
-                    }}
-                    text="Start - End Date"
-                  />
-                  <GrayMediumText
-                    _style={{
-                      color: colors.text.black,
-                    }}
-                    text={`${format(edu.start, 'yyyy-MM-dd')} - ${isAfter(new Date(), edu.end) ? format(edu.end, 'yyyy-MM-dd') : 'Present'}`}
-                  />
-                </View>
-              </View>
-            </View>
-          ))}
-          <View style={styles.actionsBtnView}>
+              ))}
+            </View> :
+            <View style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              padding: 20
+            }}>
+              <GrayMediumText
+                _style={{
+                  color: colors.theme.greyAlt2,
+                }}
+                text="You haven't added any education"
+              />
+            </View>}
+          <View style={[styles.actionsBtnView]}>
             <AppButton
               btnStyle={styles.actionBtn}
-              title="Edit Education"
+              title={profile.education.length ? "Edit Education" : "Add Education"}
               onPress={() => navigation.navigate(EProfileStack.editEducation)}
             />
           </View>
@@ -267,80 +282,95 @@ const Profile = ({ navigation }: Props) => {
             }}
             text="Experience"
           />
-          {profile.employment?.map((emp, ind) => (
-            <View key={'exp-' + ind}>
-              {ind > 0 && (
-                <View
-                  style={
-                    [styles.divider, {
-                    }]
-                  }
-                />
-              )}
-              <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
-                <View style={styles.userInfoItem}>
-                  <GrayMediumText
-                    _style={{
-                      color: colors.theme.primary,
-                    }}
-                    text="Office Name"
-                  />
-                  <GrayMediumText
-                    _style={{
-                      color: colors.text.black,
-                    }}
-                    text={emp.school}
-                  />
+          {profile.employment.length ?
+            <View style={{ marginBottom: 50 }}>
+              {profile.employment?.map((emp, ind) => (
+                <View key={'exp-' + ind}>
+                  {ind > 0 && (
+                    <View
+                      style={
+                        [styles.divider, {
+                        }]
+                      }
+                    />
+                  )}
+                  <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+                    <View style={styles.userInfoItem}>
+                      <GrayMediumText
+                        _style={{
+                          color: colors.theme.primary,
+                        }}
+                        text="Office Name"
+                      />
+                      <GrayMediumText
+                        _style={{
+                          color: colors.text.black,
+                        }}
+                        text={emp.school}
+                      />
+                    </View>
+                    <View style={styles.userInfoItem}>
+                      <GrayMediumText
+                        _style={{
+                          color: colors.theme.primary,
+                        }}
+                        text="Job Title"
+                      />
+                      <GrayMediumText
+                        _style={{
+                          color: colors.text.black,
+                        }}
+                        text={emp.position}
+                      />
+                    </View>
+                    <View style={styles.userInfoItem}>
+                      <GrayMediumText
+                        _style={{
+                          color: colors.theme.primary,
+                        }}
+                        text="Start - End Date"
+                      />
+                      <GrayMediumText
+                        _style={{
+                          color: colors.text.black,
+                        }}
+                        text={`${format(emp.start, 'yyyy-MM-dd')} - ${isAfter(new Date(), emp.end) ? format(emp.end, 'yyyy-MM-dd') : 'Present'}`}
+                      />
+                    </View>
+                    <View style={styles.userInfoItem}>
+                      <GrayMediumText
+                        _style={{
+                          color: colors.theme.primary,
+                        }}
+                        text="Address"
+                      />
+                      <GrayMediumText
+                        _style={{
+                          color: colors.text.black,
+                        }}
+                        text={emp.address}
+                      />
+                    </View>
+                  </View>
                 </View>
-                <View style={styles.userInfoItem}>
-                  <GrayMediumText
-                    _style={{
-                      color: colors.theme.primary,
-                    }}
-                    text="Job Title"
-                  />
-                  <GrayMediumText
-                    _style={{
-                      color: colors.text.black,
-                    }}
-                    text={emp.position}
-                  />
-                </View>
-                <View style={styles.userInfoItem}>
-                  <GrayMediumText
-                    _style={{
-                      color: colors.theme.primary,
-                    }}
-                    text="Start - End Date"
-                  />
-                  <GrayMediumText
-                    _style={{
-                      color: colors.text.black,
-                    }}
-                    text={`${format(emp.start, 'yyyy-MM-dd')} - ${isAfter(new Date(), emp.end) ? format(emp.end, 'yyyy-MM-dd') : 'Present'}`}
-                  />
-                </View>
-                <View style={styles.userInfoItem}>
-                  <GrayMediumText
-                    _style={{
-                      color: colors.theme.primary,
-                    }}
-                    text="Address"
-                  />
-                  <GrayMediumText
-                    _style={{
-                      color: colors.text.black,
-                    }}
-                    text={emp.address}
-                  />
-                </View>
-              </View>
-            </View>
-          ))}
+              ))}
+            </View> :
+            <View style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              padding: 20
+            }}>
+              <GrayMediumText
+                _style={{
+                  color: colors.theme.greyAlt2,
+                }}
+                text="You haven't added any experience"
+              />
+            </View>}
           <View style={styles.actionsBtnView}>
             <AppButton
               btnStyle={styles.actionBtn}
-              title="Edit Experience"
+              title={profile.employment.length ? "Edit Experience" : "Add Experience"}
               onPress={() => navigation.navigate(EProfileStack.editExperience)}
             />
           </View>
@@ -378,6 +408,7 @@ const styles = StyleSheet.create({
   userInfo: {
     flexDirection: 'row',
     flexWrap: 'wrap',
+    marginBottom: 50
   },
   userInfoItem: {
     minWidth: '50%',
@@ -394,7 +425,7 @@ const styles = StyleSheet.create({
   },
   actionsBtnView: {
     alignItems: 'center',
-    marginTop: 50,
+    // marginTop: 50,
   },
   actionBtn: {
     flex: 1,
