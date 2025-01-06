0c6ca74f5590e875da3ecb1910e6bd55cce946ed
Zaid Bin Ahmed Makharish <zaidahmed332675@gmail.com>
2025-01-06 19:54:04 +0500

    Fixes in FlatList loadmore

--- src/Components/LoadMoreFlatList/index.tsx
+++ src/Components/LoadMoreFlatList/index.tsx
@@ -1,10 +1,10 @@
 import React, { useRef, useState } from 'react';
 import {
-    ListRenderItem
+    FlatList,
+    ListRenderItem,
+    RefreshControl
 } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
 import { LoadIndicator } from '../../Components/LoadIndicator';
-import { RefreshControl } from 'react-native';
 
 interface LoadMoreFlatListProps {
     uuidKey: string;
@@ -12,14 +12,15 @@ interface LoadMoreFlatListProps {
     loading: boolean;
     loadMore: (isFresh?: boolean) => void;
     renderItem: ListRenderItem<any>;
+    numColumns?: number;
 }
 
-const LoadMoreFlatList = ({ uuidKey = "_id", data, loading, loadMore, renderItem }: LoadMoreFlatListProps) => {
-    const flatListRef = useRef({ listHeight: 0 });
+const LoadMoreFlatList = ({ uuidKey = "_id", data, loading, loadMore, renderItem, numColumns = 1 }: LoadMoreFlatListProps) => {
+    const [layoutSize, setLayoutSize] = useState(0)
     const [contentSize, setContentSize] = useState(0)
     const [refreshing, setRefreshing] = useState(false);
 
-    const isScrollable = contentSize > flatListRef.current.listHeight
+    const isScrollable = contentSize > layoutSize
 
     const onRefresh = () => {
         setRefreshing(true);
@@ -32,21 +33,23 @@ const LoadMoreFlatList = ({ uuidKey = "_id", data, loading, loadMore, renderItem
     return (
         <FlatList
             data={data}
-            keyExtractor={item => item[uuidKey]}
+            numColumns={numColumns}
+            keyExtractor={item => item[uuidKey].toString()}
             initialNumToRender={10}
             maxToRenderPerBatch={10}
             windowSize={5}
             renderItem={renderItem}
+            keyboardShouldPersistTaps="handled"
             onEndReached={({ distanceFromEnd }) => {
-                if (isScrollable && distanceFromEnd >= 0 && !!flatListRef.current.listHeight) loadMore()
+                if (isScrollable && distanceFromEnd >= 0 && !!layoutSize) loadMore()
             }}
             onContentSizeChange={(_, height) => {
-                if (flatListRef.current.listHeight == 0) {
+                if (layoutSize == 0) {
                     setContentSize(height)
                 }
             }}
             onLayout={(e) => {
-                flatListRef.current = { listHeight: e.nativeEvent.layout.height }
+                setLayoutSize(e.nativeEvent.layout.height)
             }}
             onEndReachedThreshold={0.1}
             contentContainerStyle={{
