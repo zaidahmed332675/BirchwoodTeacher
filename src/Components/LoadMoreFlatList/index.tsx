711d68d73dc3bbf1520cfffca26d91bf47e2453f
Zaid Bin Ahmed Makharish <zaidahmed332675@gmail.com>
2025-01-06 17:17:07 +0500

    Pull to scroll integrated

--- src/Components/LoadMoreFlatList/index.tsx
+++ src/Components/LoadMoreFlatList/index.tsx
@@ -4,20 +4,30 @@ import {
 } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { LoadIndicator } from '../../Components/LoadIndicator';
+import { RefreshControl } from 'react-native';
 
 interface LoadMoreFlatListProps {
     uuidKey: string;
     data: ArrayLike<any>;
     loading: boolean;
-    loadMore: () => void;
+    loadMore: (isFresh?: boolean) => void;
     renderItem: ListRenderItem<any>;
 }
 
 const LoadMoreFlatList = ({ uuidKey = "_id", data, loading, loadMore, renderItem }: LoadMoreFlatListProps) => {
     const flatListRef = useRef({ listHeight: 0 });
     const [contentSize, setContentSize] = useState(0)
+    const [refreshing, setRefreshing] = useState(false);
 
     const isScrollable = contentSize > flatListRef.current.listHeight
+
+    const onRefresh = () => {
+        setRefreshing(true);
+        loadMore(true)
+        setTimeout(() => {
+            setRefreshing(false);
+        }, 2000);
+    };
 
     return (
         <FlatList
@@ -40,9 +50,13 @@ const LoadMoreFlatList = ({ uuidKey = "_id", data, loading, loadMore, renderItem
             }}
             onEndReachedThreshold={0.1}
             contentContainerStyle={{
+                flexGrow: 1,
                 paddingBottom: 10,
             }}
             ListFooterComponent={() => data.length && loading ? <LoadIndicator /> : null}
+            refreshControl={
+                <RefreshControl enabled={true} refreshing={refreshing} onRefresh={onRefresh} />
+            }
         />
     );
 };
